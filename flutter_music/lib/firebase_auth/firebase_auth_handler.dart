cb4719538c96f770e7cc5709537a5084b2c28f9b
Pranay3311 <patilpranay3311@gmail.com>
2021-04-14 22:44:56 +0530

    firebase_auth_handler.dart modified

--- flutter_music/lib/firebase_auth/firebase_auth_handler.dart
+++ flutter_music/lib/firebase_auth/firebase_auth_handler.dart
@@ -4,7 +4,6 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
-
 Future<User> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
