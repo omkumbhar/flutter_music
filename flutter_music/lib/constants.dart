4d83a6e57632db9ed4d0e3dcc53f8795d498afa3
omkumbhar <om.kumbhar1998@gmail.com>
2021-04-14 22:53:07 +0530

    update constant file

--- flutter_music/lib/constants.dart
+++ flutter_music/lib/constants.dart
@@ -7,6 +7,8 @@ import 'players/local_song_player/player.dart';
 import 'players/online_song_player/onilne_player.dart';
 
 
+
+
 bool isLocalPlayed = true;
 final AudioPlayer audioPlayer = AudioPlayer();
 final SongPlayer songPlayer = SongPlayer();
